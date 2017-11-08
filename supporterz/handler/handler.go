c8a704edf6d35b75664bfcd1279c85dcc2eec464
Kazumasa Shimomura <kazu.devapp@gmail.com>
2017-11-09 00:26:11 +0900

    print json

--- supporterz/handler/handler.go
+++ supporterz/handler/handler.go
@@ -3,10 +3,30 @@ package handler
 import (
 	"net/http"
 
+	"io/ioutil"
+	"log"
+
+	"encoding/json"
+
 	"github.com/gin-gonic/gin"
 )
 
+type Request struct {
+	Order []string `json:"order"`
+}
+
 func CheckoutHandler(c *gin.Context) {
+
+	bytes, _ := ioutil.ReadAll(c.Request.Body)
+	log.Printf("%s", bytes)
+
+	var request Request
+
+	if err := json.Unmarshal(bytes, &request); err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("order[0]: %s\n", request.Order[0])
 
 	c.String(http.StatusOK, "OK")
 }
